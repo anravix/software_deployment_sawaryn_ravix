f22e6cf85b77f005acf63b293067beb11baa74c9
anravix <ravix.anne2@gmail.com>
2020-03-26 10:26:25 +0100

    ajout doctests

--- vectors_SP.py
+++ vectors_SP.py
@@ -1,8 +1,3 @@
-""" 
-Realisation des fonctions
-========================
-"""
-
 def add_vectors(vector1,vector2):
     """Creation of a vector by the addition of two others. 
 
@@ -20,7 +15,15 @@ def add_vectors(vector1,vector2):
     Returns
     -------
     list
-        Sum of the corresponding  elements of each list. 
+        Sum of the corresponding  elements of each list.
+
+    Examples
+    --------
+    >>> add_vectors([1, 2, 3], [1, 2, 5])
+    [2, 4, 8]
+
+    >>> add_vectors([-1, 3, 0, 6, 7], [0, 7, 10, -4, 3])
+    [-1, 10, 10, 2, 10]
 
     """
     mat = []
@@ -43,6 +46,17 @@ def scalar_mult(scalar, vector):
     -------
     list
         The multiplication of the corresponding  elements of the list by a scalar. 
+
+    Examples
+    --------
+    >>> scalar_mult(5, [1,2])
+    [5, 10]
+
+    >>> scalar_mult(3, [1,0,-1])
+    [3, 0, -3]
+
+    >>> scalar_mult(7, [3,0,5,11,2])
+    [21, 0, 35, 77, 14]
 
     """
     mat =[]
@@ -70,11 +84,23 @@ def dot_product(vec1,vec2):
     int
         The sum of the products of the corresponding elements of each.
 
+    Examples
+    --------
+    >>> dot_product([1,1],[1,1])
+    2
+
+    >>> dot_product([1,2],[1,4])
+    9
+
+    >>> dot_product([1,2,1],[1,4,3])
+    12
+
     """
     count=0
     for i in range (len(vec1)):
         count+=vec1[i]*vec2[i]
     return count
+
 
 def replace(s,old,new):
     """Replace all occurences of old with new in the string s.
@@ -93,6 +119,14 @@ def replace(s,old,new):
     string
         The modified list on each pattern. 
 
+    Examples
+    --------
+    >>> replace("Mississippi","i","I")
+    MIssIssIppI
+
+    >>> replace("I love spom! Spom is my favorite food. Spom, spom, yum!","om","am")
+    I love spam! Spam is my favorite food. Spam, spam, yum!
+ 
     """
     liste=s.split(old)
     glue=new
@@ -116,6 +150,12 @@ def nb_occurence(chaine):
 	dictionnary
         The sort dictionnary by alphabetical order of the letters of the chain and their ocurrence in the chain. 
 
+    Examples
+    --------
+    >>> nb_occurence("ThiS is String with Upper and lower case Letters")
+    {' ': 8, 'a': 2, 'c': 1, 'd': 1, 'e': 5, 'g': 1, 'h': 2, 'i': 4, 'l': 2, 'n': 2, 'o': 1,
+    'p': 2, 'r': 4, 's': 5, 't': 5, 'u': 1, 'w': 2}
+
     """
     table={}
     for i in chaine.lower():
@@ -132,28 +172,3 @@ def nb_occurence(chaine):
             table_trie[i[0]] = i[j]
 
     return table_trie
-
-"""
-Tests unitaires
----------------
-
-"""
-print(replace("Mississippi","i","I"))
-print(replace("I love spom! Spom is my favorite food. Spom, spom, yum!","om","am"))
- 
-print(scalar_mult(5, [1,2]))
-print(scalar_mult(3, [1,0,-1]))
-print(scalar_mult(7, [3,0,5,11,2]))
-
-print(add_vectors([1,1],[1,1]))
-print(add_vectors([1,2],[1,4]))
-print(add_vectors([1,2,1],[1,4,3]))
-
-print(dot_product([1,1],[1,1]))
-print(dot_product([1,2],[1,4]))
-print(dot_product([1,2,1],[1,4,3]))
-#CECI EST UN COMMENTAIRE
-
-
-print(nb_occurence("ThiS is String with Upper and lower case Letters"))
-
